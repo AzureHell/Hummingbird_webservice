c2aea61b18f756185b33b250444e03312ee7af08
Lord Huang <Lanca@LordtekiMacBook-Air.local>
2012-11-17 00:46:22 +0800

    first

---
+++ dbHelper.py
@@ -0,0 +1,131 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# Created on 2011-10-01
+import pyodbc
+import json
+import time
+
+def getConn():
+    return pyodbc.connect('DRIVER={SQL Server};Server=.;DATABASE=qchelper;UID=qchelper;PWD=1qaz2wsx')
+
+def sqlQuery(conn, sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    x = cursor.fetchall()
+    #cursor.close()
+    conn.close()
+    return x
+def sqlExcute(conn, sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    #cursor.close()
+    conn.close()
+    return 1
+
+
+def getInfo():
+    recSet = sqlQuery(getConn(), "select top 1 user_name, password from dbo.users")
+
+    print("中文".encode('utf8'))
+    
+    for recOne in recSet:
+        return recOne[0] + " : " + recOne[1] + ',defaultencoding:' + sys.getdefaultencoding()
+
+def checkUser(username, password):
+    recSet = sqlQuery(getConn(), "select top 1 user_id,user_name from dbo.users where user_name = '%s' and password = '%s'"%(username, password))    
+    if recSet.count > 0:
+        #1 rows only
+        for recOne in recSet:
+            return '{"user_id":"'+str(recOne.user_id)+'","user_name":"'+recOne.user_name+'"}'
+    else:
+        return 'error:not return count!'
+
+def downloadCheckPlan(sQCUserID, iID):
+    jsonresult = ''
+    record_count = 0
+    sql = "select iID, iFactoryID, sOrderNo, sStyleNo, sProductID, dRequestCheck, sCheckItemDesc, sQCUserID, sUserID from qmCheckPlan where sQCUserID = '%s' and iID > %s"%(sQCUserID, iID)
+    recSet = sqlQuery(getConn(), sql)
+    for recOne in recSet:
+        stra = '{"iID":"'+str(recOne.iID)+'","iFactoryID":"'+str(recOne.iFactoryID)+'","sOrderNo":"'+recOne.sOrderNo+'","sStyleNo":"'+recOne.sStyleNo+'","sProductID":"' \
+            +recOne.sProductID+'","dRequestCheck":"'+str(recOne.dRequestCheck)+'","sCheckItemDesc":"'+recOne.sCheckItemDesc.decode( "GB2312")+'","sQCUserID":"'+str(recOne.sQCUserID)+'","sUserID":"'+str(recOne.sUserID)+'"}'
+        if jsonresult <> '':
+            jsonresult += ',' + stra
+        else:
+            jsonresult = stra
+        record_count += 1
+    jsonresult = '{"table":"qmCheckPlan","count":"'+str(record_count)+'","records":['+jsonresult+']}'
+    return jsonresult
+
+def fieldisNull(x):
+    if x == 'null':
+        return x
+    else:
+        return "'" + x + "'"
+
+def uploadCheckRecord(masterDict, detailCount, detailDict):
+    sql = "declare @MstID table (iMstID int);\n"
+    sql += "insert into [dbo].[qmCheckRecordMst](iFactoryID, sOrderNo, sStyleNo, sProductID "
+    sql += ", iItemID, dChecdedDate, sRemark, datetime_rec, datetime_delete, datetime_upload, id_upload, user_id_opt "
+    sql += ") output inserted.iID into @MstID \n"
+    sql += "select " + fieldisNull(masterDict[0]['iFactoryID'])
+    sql += "," + fieldisNull(masterDict[0]['sOrderNo'])
+    sql += "," + fieldisNull(masterDict[0]['sStyleNo'])
+    sql += "," + fieldisNull(masterDict[0]['sProductID'])
+    sql += "," + fieldisNull(masterDict[0]['iItemID'])
+    sql += "," + fieldisNull(masterDict[0]['dChecdedDate'])
+    sql += "," + fieldisNull(masterDict[0]['sRemark'])
+    sql += "," + fieldisNull(masterDict[0]['datetime_rec'])
+    sql += "," + fieldisNull(masterDict[0]['datetime_delete'])
+    sql += ",'" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    sql += "'," + fieldisNull(masterDict[0]['iID'])
+    sql += "," + fieldisNull(masterDict[0]['user_id_opt'])
+    sql += ";\n"
+    
+    for i in range(0, detailCount):
+        sql += "insert into [dbo].[qmCheckRecordDtl](iID, iMstID, sFileName, dCreateDate, datetime_delete) \n "
+        sql += "select " + fieldisNull(detailDict[i]['iID'])
+        sql += ", (select top 1 iMstID from @MstID) as iMstID "
+        sql += "," + fieldisNull(detailDict[i]['sFileName'])
+        sql += "," + fieldisNull(detailDict[i]['dCreaimestamp'])
+        sql += "," + fieldisNull(detailDict[i]['datetime_delete'])
+        sql += ";\n"
+        
+    print(sql)
+    sqlExcute(getConn(), sql)
+
+    sql = "select top 1 id_upload, user_id_opt, datetime_upload from dbo.qmCheckRecordMst with(nolock) \n"
+    sql += " where user_id_opt = " + masterDict[0]["user_id_opt"]
+    sql += " and id_upload = " + masterDict[0]["iID"]
+    sql += " order by datetime_upload desc;\n"
+    print(sql)
+    recSet = sqlQuery(getConn(), sql)
+    for recOne in recSet:
+        result = '{"iID":"' + str(recOne.id_upload) + '", "user_id_opt":"' + str(recOne.user_id_opt) + '", "datetime_upload":"' + str(recOne.datetime_upload) + '"}'
+        result = '{"table":"qmCheckRecordMst","count":"1","records":['+result+']}'
+    return result
+
+def uploadCheckRecordPic(fileName, raw):
+    con = getConn()
+    cur = con.cursor()
+
+    sql = "update [dbo].[qmCheckRecordDtl] set bPhoto = ?, sFileName = sFileName + '.finished' where sFileName = ?"
+    cur.execute(sql, (pyodbc.Binary(raw),fileName))
+    con.commit()
+    con.close()
+
+    return "{}"
+
+def showPic():
+    con = getConn()
+    cur = con.cursor()
+
+    sql = "select top 1 bPhoto from dbo.qmCheckRecordDtl with(nolock)"
+    cur.execute(sql)
+    x = cur.fetchall()
+    for r in x:
+        return r.bPhoto
+    con.close()
+
+if __name__ == "__main__":
+    print(getInfo())
---
+++ index.py
@@ -0,0 +1,119 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+## Created on 2011-10-01
+from bottle import route, run, get, post, request, response
+import dbHelper
+import json
+import logging
+
+#HTTP_CODES['Request Timeout'] = 100000
+#HTTP_CODES['Gateway Timeout'] = 100000
+
+
+@route('/')
+@route('/hello')
+def hello():
+    return dbHelper.getInfo()
+
+def get_post_json():
+    try:
+        return json.load(request.body)
+    except ValueError:
+        abort(400, 'Bad request: Could not decode request body(expected JSON).')
+
+@post('/checkuser')
+def checkUser():
+    content = request.json
+    print(content)
+    try:
+        #1 rows only
+        if content['status'] == 'succeed':
+            username = content['data'][0]['username']
+            password = content['data'][0]['password']
+    except ValueError:
+        abort(400, 'Bad request: Could not decode request body(expected JSON).')
+    #print(username)
+    #print(password)
+    result = dbHelper.checkUser(username, password)
+    #print(result)
+    if result[1:6] == "error:":
+        str = fmtContent("", 1, 'failed', result[result.find(':'):])
+    else:
+        str = fmtContent(result)
+    print(str)
+    return str
+
+@post('/downloadCheckPlan')
+def downloadCheckPlan():
+    content = request.json
+    print(content)
+    try:
+        #1 rows only
+        if content['status'] == 'succeed':
+            sQCUserID = content['data'][0]['sQCUserID']
+            iID = content['data'][0]['iID']
+    except ValueError:
+        abort(400, 'Bad request: Could not decode request body(expected JSON).')
+    result = dbHelper.downloadCheckPlan(sQCUserID, iID)
+    #print(result)
+    if result[1:6] == "error:":
+        str = fmtContent("", 1, 'failed', result[result.find(':'):])
+    else:
+        str = fmtContent(result)
+    print(str)
+    #response.content_type = 'text/html; charset=utf8'
+    return str
+
+@post('/uploadCheckRecord')
+def uploadCheckRecord():
+    content = request.json
+    print(content)
+    try:
+        if content['status'] == 'succeed':
+            masterDict = content['data'][0]['records']
+            #这里需要判断是否存在[1]的数据
+            detailCount = int(content['data'][1]['count'])
+            detailDict = content['data'][1]['records']
+    except ValueError:
+        abort(400, 'Bad request: Could not decode request body(expected JSON).')
+    result = dbHelper.uploadCheckRecord(masterDict, detailCount, detailDict)
+    #print(result)
+    if result[1:6] == "error:":
+        str = fmtContent("", 1, 'failed', result[result.find(':'):])
+    else:
+        str = fmtContent(result)
+    print(str)
+    return str
+
+@post('/uploadCheckRecordPic')
+def uploadCheckRecordPic():
+    #name = request.froms.name
+ 
+    data = request.files.data
+    if data is not None:
+        print('ok')
+        raw = data.file.read() # This is dangerous for big files
+        filename = data.filename
+
+    print('filename: ' + filename)
+    filesize = len(raw)
+    print(filesize)
+    #print(type(raw))
+    result = dbHelper.uploadCheckRecordPic(filename, raw)
+    print(result)
+    if result[1:6] == "error:":
+        str = fmtContent("", 1, 'failed', result[result.find(':'):])
+    else:
+        str = fmtContent(result)
+    print(str)
+    return str
+
+@route('/showpic')
+def showpic():
+    response.set_header("Content-type", "image/PNG")
+    return dbHelper.showPic()
+
+def fmtContent(data, status='succeed', error=''):
+    return '{"status":"'+status+'","error":"'+error+'","data":['+data+']}'
+
+run(host='192.168.100.200', port=8080, debug=True)
