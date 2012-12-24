3223d95885aeef9fdf39b016d045e49ea77bdafd
hellwen <hellwen.wu@gmail.com>
2012-12-24 22:47:05 +0800

    fix

--- dbHelper.py
+++ dbHelper.py
@@ -78,7 +78,10 @@ def downloadCheckPlan(sQCUserID, iID):
     rows = sqlQuery(getConn(), sql)
     for row in rows:
         #临时用这种方法处理下
-        sCheckItemDesc = row.sCheckItemDesc[:row.sCheckItemDesc.find(u'\x00')]
+        if row.sCheckItemDesc != None:
+            sCheckItemDesc = row.sCheckItemDesc[:row.sCheckItemDesc.find(u'\x00')]
+        else:
+            sCheckItemDesc = ""
         stra = '{"iID":"'+str(row.iID)+'","iFactoryID":"'+str(row.iFactoryID)+'","sOrderNo":"'+row.sOrderNo+'","sStyleNo":"'+row.sStyleNo+'","sProductID":"' \
             +row.sProductID+'","dRequestCheck":"'+str(row.dRequestCheck)+'","sCheckItemDesc":"'+sCheckItemDesc+'","sQCUserID":"'+str(row.sQCUserID)+'","sUserID":"'+str(row.sUserID)+'","bApproved":"'+str(row.bApproved)+'"}'
         if jsonresult <> '':
